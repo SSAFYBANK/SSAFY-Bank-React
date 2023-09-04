c5ec39bc256aa101fc41ce45f0dbf3436e2d6177
Crassula1994 <gogun3535@gmail.com>
2023-09-05 04:28:50 +0900

    :hammer: [Refactor] Modify Config File

--- src/components/modal/Modal2.tsx
+++ src/components/modal/Modal2.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import styled from "@emotion/styled";
 
 interface ModalProps {
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    assetsInclude: ["**/*.png"],
     server: {
         watch: {
             usePolling: true,
         },
-
         host: true,
         strictPort: true,
         port: 5173,
