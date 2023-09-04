31c39ae7bcbf56b53a2adeec9110db92cefca08d
wns882@naver.com <wns882@naver.com>
2023-09-04 15:18:02 +0900

    :zap: feat(reissue) 토큰재발급 구현

--- src/components/info/InfoKey.tsx
+++ src/components/info/InfoKey.tsx
@@ -1,6 +1,8 @@
 /* Import */
 import styled from "@emotion/styled";
 import Theme from "@assets/styles/Theme";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -11,12 +13,48 @@ const { primary, whitegray } = Theme.colors;
 
 /* Info Key Component */
 function InfoKey() {
+    const [token, setToken] = useState(sessionStorage.getItem("Authorization"));
+
+    useEffect(() => {
+        setToken(sessionStorage.getItem("Authorization"));
+    }, []);
+    async function reIssueToken() {
+        try {
+            const oldToken = sessionStorage.getItem("Authorization");
+            const response = await axios.post(
+                `http://localhost:8081/v1/member/reissue`,
+                {},
+                {
+                    headers: {
+                        'Authorization': oldToken
+                    }
+                }
+            );
+
+            if (response.data.code === 1) {
+                const newToken = response.data.data;
+                sessionStorage.setItem("Authorization", newToken);
+                setToken(newToken);  // 상태 업데이트로 재랜더링을 유발
+            }
+
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                if (error.response?.data.code === -1) {
+                    alert(error.response.data.msg);
+                }
+            }
+            console.error('An error occurred:', error);
+        }
+    }
     return (
+        <>
         <InfoKeyContainer>
             <SubTitleWrapper>나의 API 키 확인</SubTitleWrapper>
             <KeyHeader>나의 API 키</KeyHeader>
-            <KeyWrapper>{sessionStorage.getItem("Authorization")}</KeyWrapper>
+            <KeyWrapper>{token}</KeyWrapper>
+         <DetailsDiv2 onClick={reIssueToken}>재발급</DetailsDiv2>
         </InfoKeyContainer>
+         </>
     );
 }
 
@@ -57,6 +95,18 @@ const KeyWrapper = styled("div")`
     padding: 3vh;
     word-wrap: break-word;
     background-color: ${whitegray};
+`;
+const DetailsDiv2 = styled.span`
+margin-top : 20px;
+  background-color: #ccc;
+  color: white;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #aaa;
+  }
 `;
 
 // ----------------------------------------------------------------------------------------------------
